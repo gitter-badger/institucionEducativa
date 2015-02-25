5d194c7cec24dba4482580315f7f906fd2ce78c7
BryanDaniel <bryan_slvr@hotmail.com>
2015-02-24 21:33:35 -0500

    Creamos la tabla Usuarios

---
+++ app/database/migrations/2015_02_25_020458_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration
+{
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('charter')->nullable();
+			$table->string('username');
+			$table->string('password');
+			$table->string('email');
+			$table->string('name');
+			$table->string('last_name');
+			$table->text('direction');
+			$table->integer('phone');
+			$table->integer('cellphone')->nullable();
+			$table->date('birth');
+			$table->integer('active')->default(0);
+			$table->integer('role_id')->default(1);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
