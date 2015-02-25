9d6127b43cd8507f704b5486412a96bb8456ca76
Bryan Velastegui <bryan_slvr@hotmail.com>
2015-02-25 08:26:49 -0500

    Agregamos 2 rutas

--- app/routes.php
+++ app/routes.php
@@ -3,3 +3,8 @@
 Route::get('/', function () {
 	return View::make('hello');
 });
+
+Route::get('login', ['as' => 'login', 'uses' => 'AuthController@login']);
+
+Route::get('dashboard', ['as' => 'dashboard', 'uses' => 'AuthController@auth']);
+
