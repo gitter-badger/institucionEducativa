53002a4a9511a2a5830a17cad76e381e8ae0be11
BryanDaniel <bryan_slvr@hotmail.com>
2015-02-24 21:42:57 -0500

    Agrego 4 constantes definiendo los roles para los usuarios existentes

--- app/models/User.php
+++ app/models/User.php
@@ -1,11 +1,17 @@
 <?php
 
-use Illuminate\Auth\UserTrait;
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\UserTrait;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Eloquent implements UserInterface, RemindableInterface
+{
+
+	const ROLE_STUDENT = 1;
+	const ROLE_TEACHER = 2;
+	const ROLE_FAMILY = 3;
+	const ROLE_ADMIN = 4;
 
 	use UserTrait, RemindableTrait;
 
